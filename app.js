9360bcd30a95480f9fca51ae30e9f45f0af09385
Phil Fernandez <norcal618@live.com>
2017-03-20 19:55:18 -0700

    good one

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 function truthCheck (collection, pre) {
 
+	Array.prototype.isSame = function() {
+		for (i = 1; i < this.length; i++) {
+			if (this[0] !== this[i])
+				return false;
+		}
+		return true;
+	}
+
+	var propertyType = [];
+
+	var counter = 0;
 
 
+	collection.forEach(function(e){
+		propertyType.push(e[pre]);
+		if (e[pre])
+	});
 
+	console.log(propertyType.isSame());
 
 	return pre;
 }
 
-console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat"));
