f4cfd91d7b408060d8bcdb56ce2442dd0b373676
Phil Fernandez <norcal618@live.com>
2017-03-20 20:14:49 -0700

    all but one

--- app.js
+++ app.js
@@ -9,24 +9,28 @@ function truthCheck (collection, pre) {
 	}
 
 	var propertyType = [];
+	var propertyTruthVal = [];
 
 	var counter = 0;
 
 
 	collection.forEach(function(e){
-		propertyType.push(e[pre]);
+		propertyType.push(typeof e[pre]);
+		propertyTruthVal.push(e[pre] !== 0);
 		if (e.hasOwnProperty(pre))
 			counter++;
-	});
 
+	});
 	
 
-	if (collection.length === counter && propertyType.isSame()) {
+	if (collection.length === counter && propertyType.isSame() && propertyTruthVal.isSame()) {
 		return true;
 	}
+
+		
 
 
 	return false;
 }
 
-console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age"));
